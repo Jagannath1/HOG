5e9b7ca593e9e67e1a8303df78692d51f46a4ee7
Vinay7802 <81706300+Vinay7802@users.noreply.github.com>
2023-05-10 14:53:30 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-FILE1="main2.cpp"
-FILE2="./a.out"
-DIRECTORY="cases"
-
 convert_to_seconds () {
   local time=$1
   local minutes=${time%%m*}
@@ -11,23 +6,207 @@ convert_to_seconds () {
   seconds=${seconds%%s}
   echo $(bc <<< "$minutes*60+$seconds")
 }
-# average_time $test_file $no_of_times
-average_time (){
-	local time=0
-    for ((i=1; i<=$2; i++)); do
-    	X=`(time ./a.out "$1") 2>&1| grep sys`
-		SYS_TIME=${X:4}
-		time=$(echo "$time + $(convert_to_seconds "$SYS_TIME")" | bc)
-    done
-	echo $(echo "scale=5; $time / $2" | bc)
-}
+const=20
+k=1000
+n=1000000
+increment=100000
+incrementk=10000
+max=1000000
+maxk=100000
+time_hog=0
+time_fas=0
+time_sof=0
+time_read=0
+>input.txt
+>input.fa
+g++ gen_test.cpp
+cd HOG-main
 g++ main2.cpp
->output.txt
-for file in "$DIRECTORY"/*
-do
-	if [ -f "$file" ]
-	  then
-	    echo "Running test case $file..."
-		echo "test case $file t=$(average_time $file 10)s" >> output.txt
-	  fi
+cd ..
+cd FastAPSP-master
+    make clean
+    make
+cd ..
+cd SOF-master
+    make clean
+    make
+cd ..
+for ((j=10000; j<=$maxk; j+=$incrementk)); do
+    time_fas=0
+    time_sof=0
+    time_hog=0
+    for ((i=1; i<=$const; i++)); do
+        ./a.out "$j" "$n" "n"
+        echo "testcase $i"
+        #run hog
+        cd HOG-main
+        X=`(time ./a.out "../input.txt") 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_hog=$(echo "$time_hog + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run fast APSP
+        cd FastAPSP-master
+        X=`(time ./FastAPSP "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_fas=$(echo "$time_fas + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run SOF
+        cd SOF-master
+        X=`(time ./Apsp "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_sof=$(echo "$time_sof + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run readjoiner
+        cd gt-1.6.2-Linux_x86_64-64bit-complete
+        myreadset="myreadset"
+        Y=`(time gt readjoiner prefilter -readset "$myreadset" -db "../input.fa") 2>&1| grep user`
+        SYS_TIME=${Y:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        X=`(time gt -j 1 readjoiner overlap -readset "$myreadset" -l 2) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+    done
+    >output.txt
+    echo "HOG N=$n and K=$j t= $(echo "scale=5; $time_hog / $const" | bc)">>output.txt
+    echo "FAST N=$n and K=$j t= $(echo "scale=5; $time_fas / $const" | bc)">>output.txt
+    echo "READ N=$n and K=$j t= $(echo "scale=5; $time_read / $const" | bc)">>output.txt
+    echo "SOF N=$n and K=$j t= $(echo "scale=5; $time_sof / $const" | bc)">>output.txt
+    file="N-$n-K-$j-eq-y_results.txt"
+    cp output.txt output/$file
+done
+for ((j=1000000; j<=$max; j+=$increment)); do
+    for ((i=1; i<=$const; i++)); do
+        ./a.out "$k" "$j" "n"
+        echo "testcase $i"
+        #run hog
+        cd HOG-main
+        X=`(time ./a.out "../input.txt") 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_hog=$(echo "$time_hog + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run fast APSP
+        cd FastAPSP-master
+        X=`(time ./FastAPSP "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_fas=$(echo "$time_fas + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run SOF
+        cd SOF-master
+        X=`(time ./Apsp "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_sof=$(echo "$time_sof + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run readjoiner
+        cd gt-1.6.2-Linux_x86_64-64bit-complete
+        myreadset="myreadset"
+        Y=`(time gt readjoiner prefilter -readset "$myreadset" -db "../input.fa") 2>&1| grep user`
+        SYS_TIME=${Y:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        X=`(time gt -j 1 readjoiner overlap -readset "$myreadset" -l 2) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+    done
+    >output.txt
+    echo "HOG N=$j and K=$k t= $(echo "scale=5; $time_hog / $const" | bc)">>output.txt
+    echo "FAST N=$j and K=$k t= $(echo "scale=5; $time_fas / $const" | bc)">>output.txt
+    echo "READ N=$j and K=$k t= $(echo "scale=5; $time_read / $const" | bc)">>output.txt
+    echo "SOF N=$j and K=$k t= $(echo "scale=5; $time_sof / $const" | bc)">>output.txt
+    file="N-$j-K-$k-eq-n_results.txt"
+    cp output.txt output/$file
+done
+for ((j=1000; j<=$maxk; j+=$incrementk)); do
+    for ((i=1; i<=$const; i++)); do
+        ./a.out "$j" "$n" "y"
+        echo "testcase $i"
+        #run hog
+        cd HOG-main
+        X=`(time ./a.out "../input.txt") 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_hog=$(echo "$time_hog + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run fast APSP
+        cd FastAPSP-master
+        X=`(time ./FastAPSP "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_fas=$(echo "$time_fas + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run SOF
+        cd SOF-master
+        X=`(time ./Apsp "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_sof=$(echo "$time_sof + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run readjoiner
+        cd gt-1.6.2-Linux_x86_64-64bit-complete
+        myreadset="myreadset"
+        Y=`(time gt readjoiner prefilter -readset "$myreadset" -db "../input.fa") 2>&1| grep user`
+        SYS_TIME=${Y:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        X=`(time gt -j 1 readjoiner overlap -readset "$myreadset" -l 2) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+    done
+    >output.txt
+    echo "HOG N=$n and K=$j t= $(echo "scale=5; $time_hog / $const" | bc)">>output.txt
+    echo "FAST N=$n and K=$j t= $(echo "scale=5; $time_fas / $const" | bc)">>output.txt
+    echo "READ N=$n and K=$j t= $(echo "scale=5; $time_read / $const" | bc)">>output.txt
+    echo "SOF N=$n and K=$j t= $(echo "scale=5; $time_sof / $const" | bc)">>output.txt
+    file="N-$n-K-$j-eq-y_results.txt"
+    cp output.txt output/$file
+done
+for ((j=1000; j<=$maxk; j+=$incrementk)); do
+    for ((i=1; i<=$const; i++)); do
+        ./a.out "$j" "$n" "n"
+        echo "testcase $i"
+        #run hog
+        cd HOG-main
+        X=`(time ./a.out "../input.txt") 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_hog=$(echo "$time_hog + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run fast APSP
+        cd FastAPSP-master
+        X=`(time ./FastAPSP "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_fas=$(echo "$time_fas + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run SOF
+        cd SOF-master
+        X=`(time ./Apsp "../input.txt" -p 1) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_sof=$(echo "$time_sof + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+
+        #run readjoiner
+        cd gt-1.6.2-Linux_x86_64-64bit-complete
+        myreadset="myreadset"
+        Y=`(time gt readjoiner prefilter -readset "$myreadset" -db "../input.fa") 2>&1| grep user`
+        SYS_TIME=${Y:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        X=`(time gt -j 1 readjoiner overlap -readset "$myreadset" -l 2) 2>&1| grep user`
+        SYS_TIME=${X:5}
+        time_read=$(echo "$time_read + $(convert_to_seconds "$SYS_TIME")" | bc)
+        cd ..
+    done
+    >output.txt
+    echo "HOG N=$n and K=$j t= $(echo "scale=5; $time_hog / $const" | bc)">>output.txt
+    echo "FAST N=$n and K=$j t= $(echo "scale=5; $time_fas / $const" | bc)">>output.txt
+    echo "READ N=$n and K=$j t= $(echo "scale=5; $time_read / $const" | bc)">>output.txt
+    echo "SOF N=$n and K=$j t= $(echo "scale=5; $time_sof / $const" | bc)">>output.txt
+    file="N-$n-K-$j-eq-n_results.txt"
+    cp output.txt output/$file
 done
